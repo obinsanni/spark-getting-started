a3101b24f47a8f97267494886a76c0a9c657473e
waynemadams <wayne.adams@adamsresearch.com>
2015-02-19 23:22:47 -0700

    Committed Part 2 of tutorial

--- src/main/scala/com/example/spark/demo/SparkDemo.scala
+++ src/main/scala/com/example/spark/demo/SparkDemo.scala
@@ -1,5 +1,10 @@
 package com.example.spark.demo
 
+import scala.io.Source
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
@@ -14,9 +19,39 @@ object SparkDemo {
   def main(args: Array[String]): Unit = {
     val sampleRdd = sc.parallelize(List(1,2,3,4,5,6))
     println("Length of sample RDD is " + sampleRdd.count)
+
+    val waterData = getWaterQualityData("usgs_09085100.txt")
+    val sum = waterData.aggregate((0, 0))((acc, value) => (acc._1 + value._3, acc._2 + 1),
+      (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
+    val avgDischargeRate = sum._1/sum._2
+    println("Average discharge rate over the 4-year period: " + avgDischargeRate + " cf/sec")
+    sc.stop()
   }
 
   def getIntRdd (length: Int): RDD[Int] = {
     sc.parallelize(1 to length)
+  }
+
+  // assume file somewhere on the classpath:
+  def getWaterQualityData(filename: String): RDD[(String, Date, Int, Float, Float)] = {
+    val lines = Source.fromInputStream(this.getClass().getClassLoader().getResourceAsStream(filename)).getLines()
+    // we could skip some number of lines, or we could just notice that the data
+    // lines start with "USGS":
+    val dataLines = lines.filter(line => line.startsWith("USGS"))
+    // not all data is available on all lines, so we just skip the lines
+    // that are not complete, with a special filter function that just looks
+    // at the size of the split line:
+    val fullDataLines = dataLines.filter(line => {
+      val fields = line.split("\\\t", 9)
+      fields.length == 9
+    })
+    sc.parallelize(fullDataLines.map(nextLine => extractTuple(nextLine)).toList)
+  }
+
+  def extractTuple(line: String): (String, Date, Int, Float, Float) = {
+    // string is tab-delimited:
+    val fields = line.split("\\\t", 9)
+    (fields(1), new SimpleDateFormat("yyyy-MM-dd").parse(fields(2)), fields(3).toInt,
+      fields(5).toFloat, fields(7).toFloat)
   }
 }
--- src/test/scala/com/example/spark/demo/SparkDemoTest.scala
+++ src/test/scala/com/example/spark/demo/SparkDemoTest.scala
@@ -1,5 +1,7 @@
 package com.example.spark.demo
 
+import java.text.SimpleDateFormat
+
 import org.scalatest.FunSpec
 
 /**
@@ -21,6 +23,42 @@ class SparkDemoTest extends FunSpec {
         val prod = intRdd.aggregate((1, 0))((acc, value) => (acc._1 * value, acc._2 + 1),
           (acc1, acc2) => (acc1._1 * acc2._1, acc1._2 + acc2._2))
         assert(prod._1 === 720)
+      }
+    }
+  }
+
+  describe("The extractTuple() method") {
+    describe("When given an incomplete line of data") {
+      it("should throw an ArrayIndexOutOfBoundsException") {
+        intercept[ArrayIndexOutOfBoundsException] {
+          val incompleteLine = "USGS\t09085100\t1980-05-04\t5290\tA"
+          val shouldFail = SparkDemo.extractTuple(incompleteLine)
+        }
+      }
+    }
+    describe("When given a complete line of sample data") {
+      it("should parse the line correctly") {
+        val completeLine = "USGS\t09085100\t1980-05-03\t5020\tA\t9.0\tA\t7.5\tA"
+        val measurementDate = new SimpleDateFormat("yyyy-MM-dd").parse("1980-05-03")
+        val parsedTuple = SparkDemo.extractTuple(completeLine)
+        assert(parsedTuple._1 === "09085100" &&
+          parsedTuple._2.compareTo(measurementDate) === 0 &&
+          parsedTuple._3 === 5020 &&
+          parsedTuple._4.compareTo("9.0".toFloat) === 0 &&
+          parsedTuple._5.compareTo("7.5".toFloat) === 0)
+      }
+    }
+  }
+
+  describe("The getWaterQualityData() method") {
+    describe("when given the sample file") {
+      val dataSet = SparkDemo.getWaterQualityData("usgs_09085100.txt")
+      it("should return 1487 complete records") {
+        assert(dataSet.count === 1487)
+      }
+      it("should only contain records for sensor 09085100") {
+        val badLines = dataSet.filter(tuple => tuple._1 != "09085100")
+        assert(badLines.count === 0)
       }
     }
   }
