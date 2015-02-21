39266b6867d53e2e5e31c865c6c33e2e178ee299
waynemadams <wayne.adams@adamsresearch.com>
2015-02-20 17:52:22 -0700

    Committed Part 3 of tutorial

--- build.sbt
+++ build.sbt
@@ -7,3 +7,7 @@ scalaVersion := "2.10.4"
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.2.1"
+
+libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.5.1"
+
+libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "1.2.0-alpha2"
--- src/main/scala/com/example/spark/demo/SparkDemo.scala
+++ src/main/scala/com/example/spark/demo/SparkDemo.scala
@@ -5,6 +5,7 @@ import scala.io.Source
 import java.text.SimpleDateFormat
 import java.util.Date
 
+import com.datastax.spark.connector._
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
@@ -13,7 +14,8 @@ import org.apache.spark.rdd.RDD
  * Created by wma on 2/18/15.
  */
 object SparkDemo {
-  val conf = new SparkConf().setMaster("local").setAppName("SparkDemo")
+//  val conf = new SparkConf().setMaster("local").setAppName("SparkDemo")
+  val conf = new SparkConf(true).setMaster("local").setAppName("SparkDemo").set("spark.cassandra.connection.host", "127.0.0.1")
   val sc = new SparkContext(conf)
 
   def main(args: Array[String]): Unit = {
@@ -25,6 +27,29 @@ object SparkDemo {
       (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
     val avgDischargeRate = sum._1/sum._2
     println("Average discharge rate over the 4-year period: " + avgDischargeRate + " cf/sec")
+
+    // create a couple of output files:
+    waterData.saveAsObjectFile("waterDataObj")
+    waterData.saveAsTextFile("waterDataTxt")
+
+    val objRdd = sc.objectFile("waterDataObj")
+    println("\ncount of reconstituted object RDD: " + objRdd.count)
+    val txtRdd = sc.textFile("waterDataTxt")
+    println("count of reconstituted text RDD: " + txtRdd.count)
+
+    println("repeat, using HDFS:")
+    waterData.saveAsObjectFile("hdfs://localhost:9000/kitchenSink/waterDataObj")
+    waterData.saveAsTextFile("hdfs://localhost:9000/kitchenSink/waterDataTxt")
+    val hdfsObjRdd = sc.objectFile("hdfs://localhost:9000/kitchenSink/waterDataObj")
+    println("  count of object RDD reconstituted from HDFS: " + hdfsObjRdd.count)
+    val hdfsTxtRdd = sc.textFile("hdfs://localhost:9000/kitchenSink/waterDataTxt")
+    println("  count of text RDD reconstituted from HDFS: " + hdfsTxtRdd.count)
+
+    println("one more time, this time using Cassandra:")
+    waterData.saveToCassandra("waterquality", "nwis", SomeColumns("gaugelabel", "measurementdate", "dischargerate", "maxtempcelsius", "mintempcelsius"))
+    val cassandraRdd = sc.cassandraTable("waterquality", "nwis")
+    println("  count of RDD retrieved from Cassandra: " + cassandraRdd.count)
+
     sc.stop()
   }
 
